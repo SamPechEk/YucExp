0f0db34dc5754b8093a554a5a805b8d7d11beac9
YAIR DE JESUS CHAN MENA <yair.chan@stranet.com.mx>
2023-09-22 15:54:41 -0600

    Diseño de listados

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
 import AcmeLogoComponent from "./components/AcmeLogoComponent";
 import SearchIconComponent from "./components/SearchIconComponent";
-=======
->>>>>>> f840fca7f6899c7a658d7b2008613f2777461e24
-
 
 export default function App() {
   return (

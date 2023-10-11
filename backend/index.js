1acd8862043c26f6882b0e5b8aabbf2261d8ee4a
Samuel Pech Ek <samue-pech-ek@hotmail.com>
2023-10-10 23:26:02 -0600

    Express Inicial

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+import  express from 'express';
+
+app.listen(4000 , () =>{
+    console.log('Servidor YucExp Corriendo en el puerto 4000.')
+})

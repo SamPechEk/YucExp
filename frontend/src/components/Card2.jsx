98a0387939b0c65574c7b117779dfff06c22e026
Samuel Pech Ek <samue-pech-ek@hotmail.com>
2023-11-08 07:51:08 -0600

    PROCESO DE RESERVA

--- frontend/src/components/Card2.jsx
+++ frontend/src/components/Card2.jsx
@@ -6,29 +6,49 @@ import { Link } from "react-router-dom";
 import { Button } from "@nextui-org/react";
 import axios from 'axios';
 import { useState } from 'react';
-
+import  Swal  from 'sweetalert2';
 const Card2 = ({ rest2 }) => {
-
-
-  const [datosItemCarrito, setDatosItemCarrito] = useState({});
-
-  const handleAddToCart = () => {
-  // Realizar la solicitud POST al backend cuando se hace hace el OnClick
-  axios.post('http://localhost:7000/api/card', {
-    item: rest2,  // Envía el item del servicio al backend
+  const token = localStorage.getItem('token');
+  const usuarioLogueado = token !== null;
+  const storedMunicipio = localStorage.getItem("selectedMunicipio");
+ 
     
-  })
+ 
+  const handleAddToCart = (tabla,idservicio) => {
+  if (tabla) {
+    const data = {
+      idusuario: token,
+      tabla: tabla,
+      idservicio: idservicio,
+      idMunicipio : storedMunicipio
+    };
+
+    axios
+    .post('http://localhost:7000/api/usuarios/add/car', data)
     .then((response) => {
-      console.log('Item agregado al carrito:', response.data);
-      // Realiza alguna acción adicional si es necesario
+
+      if (response.data.success) {
+        Swal.fire(response.data.msg, '', 'success')
+        
+      }
+      console.log('Respuesta del servidor:', response.data);
     })
     .catch((error) => {
-      //Mensaje de Error
-      console.error('Error al agregar el item al carrito:', error);
+      // Maneja errores de la solicitud
+      console.log(error.response.data.msg);
+      Swal.fire(error.response.data.msg, '', 'error')
     });
-};
 
+  }
+  // Realizar la solicitud POST al backend cuando se hace hace el OnClick
+  
+  };
 
+  const handletoLogin = () => {
+    
+    window.location.replace('/login');
+   
+  };
 
   // console.log(rest2);
   return (
@@ -40,9 +60,18 @@ const Card2 = ({ rest2 }) => {
       <div className='p-5 border border-b'>
         <h1 className='py-2 dark:text-white truncate'>{rest2.nombre}</h1>
         {/* <StarRating rating={rest2.rating}/> */}
-        <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" onClick={handleAddToCart(rest2.id)}>
+
+        {!usuarioLogueado ? (
+          <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" onClick={() => handletoLogin()}>
           Agregar al Carrito
         </Button>
+           ) : (
+            <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" onClick={() => handleAddToCart(rest2.tabla, rest2.idservicio)}>
+          Agregar al Carrito
+        </Button>
+            )}
+
+        
       </div>
       {/* <h3 className='p-5 text-xl'>{rest2.price}</h3> */}
       <div className='absolute top-0 dark:bg-white m-3 px-2 py-[2.5px] rounded font-bold'>

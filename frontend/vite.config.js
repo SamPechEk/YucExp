a381c45de7ba89098b82e7219c631ddc295c0068
Samuel Pech Ek <samue-pech-ek@hotmail.com>
2023-11-30 09:48:01 -0600

    staticconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     historyApiFallback: true,
+  },build: {
+    outDir: 'dist', // o el directorio que estás utilizando
+    assetsDir: 'assets',
+    // otras opciones de construcción
   },
 })
